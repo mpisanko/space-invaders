ecdc50b9705461808101c3df34463b1553a16432
Michal Pisanko <mpisanko@gmail.com>
2025-03-20 15:56:08 +0100

    add a basic test

--- test/space_invaders/space_invaders_test.clj
+++ test/space_invaders/space_invaders_test.clj
@@ -1,7 +1,22 @@
 (ns space-invaders.space-invaders-test
   (:require [clojure.test :refer :all]
+            [clojure.spec.test.alpha :as stest]
+            [clojure.string :as str]
             [space-invaders.space-invaders :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(stest/instrument `detect-invaders)
+
+(deftest detect-invaders-test
+  (testing "a single type 1 invader"
+    (let [invader-type-1
+"--o-o--o--o-o-oooo-
+-----o-----o--ooooo
+------o---o--------
+-----ooooooo----ooo
+----oo-ooo-oo----oo
+---ooooooooooo-o-o-
+---o-ooooooo-o-----
+---o-o-----o-o-----
+------oo-oo-----o--"]
+      (is (= [[[1 3] [8 14]]]
+             (detect-invaders (str/split invader-type-1 #"\n") 0))))))
